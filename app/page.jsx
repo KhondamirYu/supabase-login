ec0f55c9fc4f39996eb97f317393214c449d1fcf
Khondamir Yunusov <hanjames1995@gmail.com>
2024-10-18 17:06:15 +0500

    change;

--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,22 @@
-'use client'
-import Link from "next/link";
-import { useContext, useState } from "react";
-import { logOut } from "../utils/service";
+"use client";
+import Navbar from "@/Components/Navbar";
+import { useContext, useEffect, useState } from "react";
+import { getUser } from "../utils/service";
 import { UserContext } from "./layout";
 export default function Home() {
-  const { user, setUser } = useContext(UserContext);
-  const [isActive, setIsActive] = useState(false)
-  const handleDropdown = () => {
-    setIsActive(!isActive)
-  }
+  const { setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const access = async () => {
+      const user = await getUser();
+      setUser(user || null);
+    };
+    access();
+  }, []);
+
   return (
     <div>
-      <div className="flex gap-4 items-center justify-end mt-10 mr-10">
-
-        {user?.id ? (
-          <div on
-            className="relative flex items-center gap-2 btn bg-transparent shadow-none border cursor-pointer p-1 rounded"
-          >
-            <span onClick={handleDropdown}
-              className="font-inter text-black-30 text-base font-medium">
-              {user.user_metadata?.full_name || user.email}
-            </span>
-            <div
-              className={`absolute top-10 right-0  gap-2 bg-white rounded-xl flex-col justify-start ${isActive ? "flex" : "hidden"}`}
-            >
-              <button
-                onClick={async () => {
-                  await logOut();
-                  setUser(null);
-                }}
-                className="border p-1 rounded bg-transparent  flex items-center gap-3"
-              >
-                <p>Log out</p>
-              </button>
-            </div>
-          </div>
-        ) : (
-          <Link
-            href="/login"
-            className="border rounded p-1 text-black-30 text-base font-medium"
-          >
-            Log in
-          </Link>
-        )}
-      </div>
-
+      <Navbar />
       {/* <StoreItems /> */}
     </div>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,52 @@
+"use client";
+import { useContext, useState } from "react";
+import { UserContext } from "../../app/layout";
+import Link from "next/link";
+import { logOut } from "../../utils/service";
+
+export default function Navbar() {
+  const { user, setUser } = useContext(UserContext);
+  const [isActive, setIsActive] = useState(false);
+  const handleDropdown = () => {
+    setIsActive(!isActive);
+  };
+  return (
+    <div className="flex gap-4 items-center justify-end mt-10 mr-10">
+      {user?.id ? (
+        <div
+          on
+          className="relative flex items-center gap-2 btn bg-transparent shadow-none border cursor-pointer p-1 rounded"
+        >
+          <span
+            onClick={handleDropdown}
+            className="font-inter text-black-30 text-base font-medium"
+          >
+            {user.user_metadata?.full_name || user.email}
+          </span>
+          <div
+            className={`absolute top-10 right-0  gap-2 bg-white rounded-xl flex-col justify-start ${
+              isActive ? "flex" : "hidden"
+            }`}
+          >
+            <button
+              onClick={async () => {
+                await logOut();
+                setUser(null);
+              }}
+              className="border p-1 rounded bg-transparent  flex items-center gap-3"
+            >
+              <p>Log out</p>
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Link
+          href="/login"
+          className="border rounded p-1 text-black-30 text-base font-medium"
+        >
+          Log in
+        </Link>
+      )}
+    </div>
+  );
+}
